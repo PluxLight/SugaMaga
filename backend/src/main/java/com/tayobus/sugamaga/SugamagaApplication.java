3c7cb3bfa036e6702d4d360af1ef3c953d5c21d3
SangHa Jeon <aeoragy@gmail.com>
2023-03-16 14:27:32 +0900

    [BE] create : Base File
    백엔드 스프링부트 기본설정
    
    
    Former-commit-id: f0ac3ff38ea0e2001bea2725160c7cb2be16508b

---
+++ backend/src/main/java/com/tayobus/sugamaga/SugamagaApplication.java
@@ -0,0 +1,13 @@
+package com.tayobus.sugamaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugamagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugamagaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tayobus/sugamaga/SugamagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tayobus.sugamaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugamagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
