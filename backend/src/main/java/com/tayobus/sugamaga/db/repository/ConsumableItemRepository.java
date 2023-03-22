72b88b05ce1dfe91f0d7fec336cda79b807df6d6
SangHa Jeon <aeoragy@gmail.com>
2023-03-22 17:55:30 +0900

    [BE] create : Drop Table
    드랍 테이블 조회 및 테스트 코드 제거
    
    
    Former-commit-id: af868e579d3af39ce4c30a6bd80031b3bf2efb9b

---
+++ backend/src/main/java/com/tayobus/sugamaga/db/repository/ConsumableItemRepository.java
@@ -0,0 +1,7 @@
+package com.tayobus.sugamaga.db.repository;
+
+import com.tayobus.sugamaga.db.entity.ConsumableItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsumableItemRepository extends JpaRepository<ConsumableItem, Integer> {
+}
