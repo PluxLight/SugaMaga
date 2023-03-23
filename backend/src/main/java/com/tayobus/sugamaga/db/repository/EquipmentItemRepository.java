ccc9dc0701596d8e8103d92963936e3cab5590bc
SangHa Jeon <aeoragy@gmail.com>
2023-03-23 12:50:24 +0900

    [BE] fix : Edit Return Value
    몬스터, 장비 아이템, 소비 아이템의
    특정 상황에서 값을 반환하는 형태를 변경
    
    
    Former-commit-id: d107390b0126afdf783d592dfa060ffefec83c39

--- backend/src/main/java/com/tayobus/sugamaga/db/repository/EquipmentItemRepository.java
+++ backend/src/main/java/com/tayobus/sugamaga/db/repository/EquipmentItemRepository.java
@@ -3,8 +3,6 @@ package com.tayobus.sugamaga.db.repository;
 import com.tayobus.sugamaga.db.entity.EquipmentItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface EquipmentItemRepository extends JpaRepository<EquipmentItem, Integer> {
-    List<EquipmentItem> findByEquipItemIdx(int equipItemIdx);
+    EquipmentItem findByEquipItemIdx(int equipItemIdx);
 }
