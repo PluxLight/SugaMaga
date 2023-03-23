ef7286a1a995aec1122cab31d52d72246e9fdba1
SangHa Jeon <aeoragy@gmail.com>
2023-03-23 09:51:10 +0900

    [BE] create : Get Game Result
    게임 결과를 조회하는 기능 구현
    
    
    Former-commit-id: 3490e6f82963be5d251b2ac769909dd701827ffa

--- backend/src/main/java/com/tayobus/sugamaga/db/repository/HistoryRepository.java
+++ backend/src/main/java/com/tayobus/sugamaga/db/repository/HistoryRepository.java
@@ -4,6 +4,9 @@ import com.tayobus.sugamaga.db.entity.History;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HistoryRepository extends JpaRepository<History, Integer> {
+    List<History> findAllByUid(String uid);
 }
