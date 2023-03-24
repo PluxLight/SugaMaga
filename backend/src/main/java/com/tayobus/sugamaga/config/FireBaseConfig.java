e5d4b267e504f087c890d1dd54033da17dc97cba
SangHa Jeon <aeoragy@gmail.com>
2023-03-24 14:21:43 +0900

    [BE] fix : File Path
    운영체제별로 파일 경로 다르게 적용
    
    
    Former-commit-id: b006c7b1140a2d731a1394f12b315b78f8a7dec0

--- backend/src/main/java/com/tayobus/sugamaga/config/FireBaseConfig.java
+++ backend/src/main/java/com/tayobus/sugamaga/config/FireBaseConfig.java
@@ -6,10 +6,8 @@ import com.google.firebase.FirebaseOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
@@ -23,21 +21,21 @@ public class FireBaseConfig {
     @PostConstruct
     public void init() throws FileNotFoundException {
         logger.info("fileDirectory : " + fileDirectory);
-        ClassPathResource cpr = new ClassPathResource("serviceAccountKey.json");
-
-        File dir = new File(fileDirectory);
-        File files[] = dir.listFiles();
-
-        for (int i = 0; i < files.length; i++) {
-            logger.info("file: " + files[i]);
-        }
-
-        String path = System.getProperty("user.dir");
-        logger.info("path : " + path);
+        String ubuntuPath = "/var/jenkins_home/workspace/SugaMaga_BE@2/backend";
+        String os = System.getProperty("os.name").toLowerCase();
+        FileInputStream serviceAccount = null;
 
         try{
-            FileInputStream serviceAccount =
-                    new FileInputStream(cpr.getFile());
+            if (os.contains("win")) {
+                serviceAccount =
+                        new FileInputStream(fileDirectory +
+                                "/src/main/resources/serviceAccountKey.json");
+            }
+            else {
+                serviceAccount =
+                        new FileInputStream(ubuntuPath +
+                                "/src/main/resources/serviceAccountKey.json");
+            }
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
