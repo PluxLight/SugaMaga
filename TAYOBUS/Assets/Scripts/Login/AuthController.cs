95256fd4ce95b2a2daa3bf9163749b68bc8426cf
SangHa Jeon <aeoragy@gmail.com>
2023-03-27 17:24:16 +0900

    [UNITY] fix : Merge Login
    로그인 씬 병합 및 유니티 토큰 미발급 이슈 대응
    
    
    Former-commit-id: de01405c9422dd7da79ddc8b3753c7b8ca0bd8e0 [formerly bc89dd865adaffcf36483166db47d13ec11ec6bb]
    Former-commit-id: b5fc6e1c0bb9191b987ce2eca8e587d64e9824d4

---
+++ TAYOBUS/Assets/Scripts/Login/AuthController.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using Firebase.Auth;
+using System;
+
+public class AuthController
+{
+    private static AuthController instance = null;
+
+    public static AuthController Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new AuthController();
+            }
+            
+            return instance;
+        }
+    }
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+
+    public string UserId => user.UserId;
+
+    public Action<bool> LoginState;
+
+    public void Init()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+
+        auth.StateChanged += OnChanged;
+    }
+
+    private void OnChanged(object sender, EventArgs e)
+    {
+        if (auth.CurrentUser != user) 
+        {
+            bool signed = (auth.CurrentUser != user && auth.CurrentUser != null);
+            if (!signed && user!= null ) 
+            {
+                Debug.Log("Logout");
+                LoginState?.Invoke(false);
+            }
+
+            user = auth.CurrentUser;
+            if (signed) 
+            {
+                Debug.Log("Login");
+                LoginState?.Invoke(true);
+            }
+        }
+    }
+
+    public void Create(string email, string password)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("is calcelded");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                //
+                Debug.LogError("is faulted");
+                return;
+            }
+
+            FirebaseUser newUser = task.Result;
+            Debug.Log("signup success");
+        });
+    }
+
+    public void Login(string email, string password)
+    {
+        Debug.Log(email);
+
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("is calcelded");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                // 
+                Debug.LogError("is faulted");
+                return;
+            }
+
+            FirebaseUser newUser = task.Result;
+            Debug.Log("signin success");
+            Debug.Log(newUser.ProviderId);
+            Debug.Log( String.Join(", ", newUser.ProviderData) );
+            Debug.Log( auth.CurrentUser.ToString() );
+        });
+    }
+
+}
