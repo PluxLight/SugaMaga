6cd701c029cee62c436701d8a47b2c31ce4bf237
SangHa Jeon <aeoragy@gmail.com>
2023-03-29 10:18:36 +0900

    [FE] add : Page Divide
    페이지 분리 작업
    css는 추후 적용 예정
    
    
    Former-commit-id: 313c7e3c8c92baeea07274b9493df6d75d4456dd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ import styled from 'styled-components';
 import Home from "./Home/Home";
 import MyPage from "./MyPage/MyPage";
 import Login from "./Sign/Login";
+import SignUp from "./Sign/SignUp";
+import History from "./History/History";
+import Download from "./Download/Download";
+import Introduce from "./Introduce/Introduce";
 
 import Header from "./Nav/Header";
 
@@ -17,17 +21,25 @@ function App() {
       <MainHeader>
         <Header />
       </MainHeader>
-      <div>
+      <MainArea>
         {router.pathname === "/" ||
+          router.pathname === "/download" ||
+          router.pathname === "/history" ||
+          router.pathname === "/introduce" ||
           router.pathname === "/mypage" ||
-          router.pathname === "/login"
+          router.pathname === "/login" ||
+          router.pathname === "/signup"
         }
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/download" element={<Download />}></Route>
+          <Route path="/history" element={<History />}></Route>
+          <Route path="/introduce" element={<Introduce />}></Route>
           <Route path="/mypage" element={<MyPage />}></Route>
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
         </Routes>
-      </div>
+      </MainArea>
     </>
   );
 }
@@ -37,9 +49,11 @@ export default App;
 const MainHeader = styled.div`
   width: 100%;
   height: 60px;
+`;
+
+const MainArea = styled.div`
+  width: 100%;
+  height: 85%;
   display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-  background: pink;
+  justify-content: center;
 `;
--- frontend/src/Nav/Header.jsx
+++ frontend/src/Nav/Header.jsx
@@ -1,7 +1,95 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
 const Header = () => {
+    const menus1 = [
+        { name: 'Home', path: '/' },
+        { name: '게임소개', path: '/introduce' },
+        { name: '게임기록', path: '/history' },
+        { name: '다운로드', path: '/download' }
+    ];
+
+    const menus2 = [
+        { name: '로그인', path: '/login' },
+        { name: '회원가입', path: '/signup' },
+        { name: '나의 정보', path: '/mypage' }
+    ];
+    
+    const FirstMenu = menus1.map((menu, index) => {
+        return (
+          <NavBarStyle
+            exact="true"
+            style={{
+              textDecoration: 'none',
+              margin: '7px',
+              marginLeft: '10px',
+              marginRight: '2px',
+            }}
+            to={menu.path}
+            key={index}
+            >
+                {menu.name}
+          </NavBarStyle>
+        );
+    });
+
+    const SecondMenu = menus2.map((menu, index) => {
+        return (
+          <NavBarStyle
+            exact="true"
+            style={{
+              textDecoration: 'none',
+              margin: '7px',
+              marginLeft: '10px',
+              marginRight: '2px',
+              marginTop: '10px',
+            }}
+            to={menu.path}
+            key={index}
+          >
+            {menu.name}
+          </NavBarStyle>
+        );
+      });
+    
     return (
-        <h1>여긴 헤더</h1>
+        <HeaderArea>
+            <Menu>{FirstMenu}</Menu>
+            <Menu2>{SecondMenu}</Menu2>
+        </HeaderArea>
     );
 };
 
 export default Header;
+
+const HeaderArea = styled.div`
+    height: 100%;
+    display: flex;
+    background: pink;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Menu = styled.div`
+
+`;
+
+const Menu2 = styled.div`
+
+`;
+
+const NavBarStyle = styled(NavLink)`
+    color: black;
+    font-size: 20px;
+    outline: invert;
+    &:link {
+    transition: 0.5s;
+    text-decoration: none;
+    }
+    &:hover {
+    color: white;
+    }
+    &.active {
+    color: HotPink;
+    }
+`;
