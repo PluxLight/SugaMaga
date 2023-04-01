67475110b2b675b63707377dd36019c00d749240
SangHa Jeon <aeoragy@gmail.com>
2023-04-02 00:11:26 +0900

    [FE] add : SideMenu
    사이드 메뉴 기능 추가
    
    
    Former-commit-id: 22c6ea983d5a7b7f1a02424947e033198b19b8fc

--- frontend/src/components/introduce/components/SideNavi.jsx
+++ frontend/src/components/introduce/components/SideNavi.jsx
@@ -1,16 +1,87 @@
 import styled from 'styled-components';
-
-import { Link } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 const SideMenu = () => {
+    const menus = [
+        { name: '- 설명', path: '/introduce/intro' },
+        { name: '- 조작법', path: '/introduce/manual' },
+        { name: '- 아트워크', path: '/introduce/artwork' }
+    ];
+
+    const sideMenu = menus.map((menu, index) => {
+        return (
+          <LinkStyle
+            exact="true"
+            style={{
+              textDecoration: 'none',
+              margin: '7px',
+              marginLeft: '10px',
+              marginRight: '2px',
+            }}
+            to={menu.path}
+            key={index}
+            >
+                {menu.name}
+          </LinkStyle>
+        );
+    });
 
     return (
-        <>
-        <Link to="/introduce">intro | </Link>
-        <Link to="/introduce/manual">manual | </Link>
-        <Link to="/introduce/artwork">artwork</Link>
-        </>
+        <NaviBox>
+
+            <HeaderStyle>게임소개</HeaderStyle>
+            <HorizonLineStyle />
+
+            {sideMenu}
+
+        </NaviBox>
     );
 };
 
 export default SideMenu;
+
+const NaviBox = styled.div`
+    background: pink;
+    width: 50%;
+    height: 20%;
+    margin-left: 25%;
+    margin-top: 15%;
+`
+
+const HeaderStyle = styled.div`
+    font-family: gyeonggi_bold;
+    font-size: 26px;
+    margin: 7px;
+    marginLeft: 10px;
+    marginRight: 2px;
+    marginTop: 10px;
+    padding-top: 10px;
+`
+
+const HorizonLineStyle = styled.div`
+    width: 90%;
+    text-align: center;
+    border-bottom: 1px solid #aaa;
+    border-color: hotpink;
+    line-height: 0.1em;
+    margin: 10px 5px 20px 5px;
+    display: flex;
+`
+
+const LinkStyle = styled(NavLink)`
+    color: black;
+    font-family: gyeonggi_bold;
+    font-size: 24px;
+    outline: invert;
+    &:link {
+    transition: 0.5s;
+    text-decoration: none;
+    }
+    &:hover {
+    color: white;
+    }
+    &.active {
+    color: HotPink;
+    }
+    display: block;
+`
