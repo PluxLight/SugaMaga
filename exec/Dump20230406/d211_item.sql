710edb44277fe33ec0f14f846e8b66542cbe4e37
SangHa Jeon <aeoragy@gmail.com>
2023-04-06 22:23:48 +0900

    [MAIN] create : Dump File
    DB Dump 파일 추가

---
+++ exec/Dump20230406/d211_item.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: aeoragy.com    Database: d211
+-- ------------------------------------------------------
+-- Server version	8.0.32-0ubuntu0.20.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `item_idx` int NOT NULL AUTO_INCREMENT,
+  `item_code` int NOT NULL,
+  `item_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`item_idx`),
+  UNIQUE KEY `item_code_UNIQUE` (`item_code`),
+  UNIQUE KEY `item_name_UNIQUE` (`item_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,100,'대검'),(2,101,'태도'),(3,102,'해머'),(4,103,'피리'),(5,104,'활'),(6,105,'보우건'),(7,106,'한손검'),(8,107,'쌍검'),(9,108,'도끼'),(10,109,'창'),(11,200,'초급 회복약'),(12,201,'중급 회복약'),(13,202,'고급 회복약'),(14,203,'최고급 회복약');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-06 22:19:47
---
+++ exec/Dump20230406/d211_user_custom.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: aeoragy.com    Database: d211
+-- ------------------------------------------------------
+-- Server version	8.0.32-0ubuntu0.20.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_custom`
+--
+
+DROP TABLE IF EXISTS `user_custom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_custom` (
+  `user_custom_idx` int NOT NULL AUTO_INCREMENT,
+  `uid` varchar(100) NOT NULL,
+  `body` int NOT NULL DEFAULT '0',
+  `ac` int NOT NULL DEFAULT '0',
+  `back` int NOT NULL DEFAULT '0',
+  `eye` int NOT NULL DEFAULT '0',
+  `hair` int NOT NULL DEFAULT '0',
+  `hat` int NOT NULL DEFAULT '0',
+  `head` int NOT NULL DEFAULT '0',
+  `mouth` int NOT NULL DEFAULT '0',
+  `eyebrow` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_custom_idx`),
+  KEY `uid_for_user_custom_idx` (`uid`),
+  CONSTRAINT `uid_for_user_custom` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=139 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_custom`
+--
+
+LOCK TABLES `user_custom` WRITE;
+/*!40000 ALTER TABLE `user_custom` DISABLE KEYS */;
+INSERT INTO `user_custom` VALUES (1,'1MNlG0QAvaXAFfYgd4p9OyjpGJq2',2,0,0,0,0,0,0,11,2),(2,'iZcds61i7NOt9wyCPS4MUkaSKsD3',11,2,1,1,9,0,1,1,0),(3,'y01Ptnb8sodLqZLSlRKdzRKWI713',0,14,3,1,11,1,19,1,1),(4,'mV3CCyfC4VQ5AzQWwgqP8VRXFs83',0,3,3,0,6,0,2,6,2),(5,'bqENGmhsmaScVJLbjj1nw6skkHm2',5,1,3,0,2,3,9,1,2),(6,'wbtDYL8ZcXXn0QrLzRjfRoANCq12',10,3,0,1,5,0,14,0,1),(76,'bpNG2yAnO2YULovCpTM0Rzq2vLG2',1,1,1,1,1,1,1,1,1),(94,'3mu2RGTbElPS4EcA9HDUu8s3ler1',17,0,2,10,9,5,3,10,2),(96,'E6MwnYo3ZzLjLRzBjykZ0NtRuYh2',6,32,4,2,9,5,17,8,1),(98,'VJNVe2KvBWc7YkJaDTs10YxA8ry2',9,25,2,11,13,12,17,3,0),(100,'l8aH0oFlt6VodB1PnnpIpG0nXWE3',15,16,4,9,5,14,8,6,2),(102,'ULA6hjyx42aPc5tbamhBypbqXJO2',17,25,2,10,0,10,9,6,1),(104,'xLXOscnBzlcsCdmaevU6JIuJxuN2',17,24,3,11,7,6,9,8,2),(106,'3V0H2DrkuJR1o6DwQ56dG7kzeYs1',5,8,6,4,12,13,12,5,2),(108,'Sb6ipofHIOWfQT4TxJGxuMwb3Gv2',10,30,6,9,11,14,15,10,2),(110,'HCYrP19URZONcdpAsUPzq6Nkve12',12,37,4,8,4,6,7,4,0),(112,'AyD5QSVP3mVVmBqIAi0aFhW9lai2',8,10,4,2,0,3,17,1,0),(114,'SbNx1rRLahg4cT7Gzw53R5imddq1',17,39,2,0,4,4,2,7,2),(116,'rqbmZyu2nBT22Y4CjfudnUcmYdA2',11,3,3,10,10,9,13,0,0),(118,'qAFzk8wMGRTmkc62AMmrrQ2fDv12',13,7,3,2,2,8,14,8,1),(120,'jHca0bCRHzgRGNO6yLHLFV3Gk673',7,33,5,4,9,0,0,1,1),(122,'qV5P1fTw3KQAG6uGdUV0ygkasaR2',5,17,3,0,6,10,2,1,0),(124,'ISwV87sH9ScyNOiY02XMbchMPfJ2',15,11,1,10,2,7,17,3,1),(126,'fg70PQkyaiRUFx8lxMlrJlaFdi32',4,29,2,2,3,6,8,1,1),(128,'g4sLUj8yncQHHPiRyfMJ8YmBUot2',18,17,5,9,1,7,13,6,1),(130,'H2LyPtLqYHfA9ohS7uqj0fiMVXa2',4,0,3,2,6,12,6,5,1),(132,'z9mofRmmLvRuVHMjIDCYkneFuj82',18,19,3,2,7,1,8,2,0),(134,'n1HHadCbZ8RC6kGkHyZFQ9UGXzE3',18,39,2,2,10,0,1,2,1),(136,'4gk6GZTBmvaF2rvdFmkdwkB1BB72',10,34,0,4,7,12,0,6,2),(138,'7jMRrFFAWFUlMxWE0M74q3bYONh2',0,2,6,7,3,8,5,2,0);
+/*!40000 ALTER TABLE `user_custom` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-06 22:19:47
